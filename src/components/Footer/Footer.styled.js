8bab12dad06a4c3dea2aaf13acb75456691c4314
Your Name <solovyova.lesya@gmail.com>
2023-10-18 21:52:15 +0300

    Update Footer.styled.js

--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -105,7 +105,6 @@ export const AddressLink = styled.a`
   @media only screen and (min-width: 1368px) {
     margin: 0px;
     font-size: 18px;
-
     text-transform: uppercase;
   }
 
@@ -220,6 +219,7 @@ export const CreatedByLink = styled.p`
 
   @media only screen and (min-width: 1368px) {
     text-align: left;
+    white-space: nowrap;
     margin: 0px;
     order: 2;
   }
