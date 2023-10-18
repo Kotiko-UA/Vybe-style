a6b0ff079f4b697f8f7d22d2a3afe5603f0ddce8
Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
2023-10-18 18:33:05 +0300

    z-index to button up

--- src/components/Btntotop/Btntottop.styled.js
+++ src/components/Btntotop/Btntottop.styled.js
@@ -1,19 +1,19 @@
 import styled from 'styled-components';
 
 export const Button = styled.button`
-display: ${props => props.$scroll === 'false' ? "none" : "block"};
-cursor:pointer;
-position: fixed;
-bottom:25px;
-right: 25px;
-width: 40px;
-height: 40px;
-border: 1px solid transparent;
-border-radius: 10px;
-background-color:${({ theme }) => theme.colorText};
-transition: all 500ms cubic-bezier(0.4, 0, 0.2, 1);
-
-&:hover {
-    background-color:${({ theme }) => theme.brandColor};
-}
-`
+  display: ${props => (props.$scroll === 'false' ? 'none' : 'block')};
+  cursor: pointer;
+  position: fixed;
+  bottom: 25px;
+  right: 25px;
+  width: 40px;
+  height: 40px;
+  border: 1px solid transparent;
+  border-radius: 10px;
+  background-color: ${({ theme }) => theme.colorText};
+  transition: all 500ms cubic-bezier(0.4, 0, 0.2, 1);
+  z-index: 999;
+  &:hover {
+    background-color: ${({ theme }) => theme.brandColor};
+  }
+`;
