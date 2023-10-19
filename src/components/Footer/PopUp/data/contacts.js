ad40230fc36e2699b66af5a5de80a5b9ff4f1da6
Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
2023-10-19 13:13:21 +0300

    del deg

--- src/components/Footer/PopUp/data/contacts.js
+++ src/components/Footer/PopUp/data/contacts.js
@@ -2,52 +2,41 @@ import yegvenPhoto from './e-aksonov-min.jpg';
 import lesyaPhoto from './photo_2023-10-16_19-34-35-min.jpg';
 import stasPhoto from './s-khrebtan-min.jpg';
 import katerunaPhoto from './photo_2023-10-17_11-29-38.jpg';
-import vladPhoto from './photo_2023-10-17_08-37-56.jpg'
 export const data = [
- {
-    id: "1",
+  {
+    id: '1',
     image: katerunaPhoto,
-    teamRole: "QA engineer",
-    teamRoleUA: "тестувальник",
-    teamNameUA: "Катерина Вторенко",
-    teamName: "Kateryna Vtorenko",
-    projectUrl: "https://www.linkedin.com/in/kateryna-vtorenko-1b5270280/"
+    teamRole: 'QA engineer',
+    teamRoleUA: 'тестувальник',
+    teamNameUA: 'Катерина Вторенко',
+    teamName: 'Kateryna Vtorenko',
+    projectUrl: 'https://www.linkedin.com/in/kateryna-vtorenko-1b5270280/',
   },
   {
-    id: "2",
+    id: '2',
     image: yegvenPhoto,
-    teamRole: "developer",
-    teamRoleUA: "розробник",
-    teamNameUA: "Євгеній Аксьонов",
-    teamName: "Aksonov Yevhen",
-    projectUrl: "https://www.linkedin.com/in/kotiko/"
+    teamRole: 'developer',
+    teamRoleUA: 'розробник',
+    teamNameUA: 'Євгеній Аксьонов',
+    teamName: 'Aksonov Yevhen',
+    projectUrl: 'https://www.linkedin.com/in/kotiko/',
   },
   {
-    id: "3",
+    id: '3',
     image: lesyaPhoto,
-    teamRole: "developer",
-    teamRoleUA: "розробник",
-    teamNameUA: "Леся Соловйова",
-    teamName: "Lesia Soloviova",
-    projectUrl: "https://www.linkedin.com/in/lesia-soloviova/"
+    teamRole: 'developer',
+    teamRoleUA: 'розробник',
+    teamNameUA: 'Леся Соловйова',
+    teamName: 'Lesia Soloviova',
+    projectUrl: 'https://www.linkedin.com/in/lesia-soloviova/',
   },
   {
-   id: "4",
+    id: '4',
     image: stasPhoto,
-    teamRole: "developer",
-    teamRoleUA: "розробник",
-    teamNameUA: "Станіслав Хребтань",
-    teamName: "Stanislav Khrebtan",
-    projectUrl: "https://linkedin.com/in/stanislav-khrebtan/"
+    teamRole: 'developer',
+    teamRoleUA: 'розробник',
+    teamNameUA: 'Станіслав Хребтань',
+    teamName: 'Stanislav Khrebtan',
+    projectUrl: 'https://linkedin.com/in/stanislav-khrebtan/',
   },
-  {
-    id: "5",
-    image: vladPhoto,
-    teamRole: "developer",
-    teamRoleUA: "розробник",
-    teamNameUA: "Владислав Марченко",
-    teamName: "Vladislav Marchenko",
-    projectUrl: "https://www.linkedin.com/in/vladm20/"
-  } 
-]
-
+];
