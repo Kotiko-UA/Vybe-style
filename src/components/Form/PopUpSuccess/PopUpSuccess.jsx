fef06e0f7e585123c8524fc9562f75f966964e0e
Your Name <solovyova.lesya@gmail.com>
2023-10-25 17:58:31 +0300

    Update PopUpSuccess.jsx

--- src/components/Form/PopUpSuccess/PopUpSuccess.jsx
+++ src/components/Form/PopUpSuccess/PopUpSuccess.jsx
@@ -1,36 +1,3 @@
-// import React, { useState } from 'react';
-// import {
-//   CloseSvgBtn,
-//   ContactMessage,
-//   PopUpSuccessContainer,
-//   SuccessBtn,
-//   SuccessMessage,
-// } from './PopUpSuccess.styled';
-// import { useTranslation } from 'react-i18next';
-
-// export const PopUpSuccess = ({ handlePopUp }) => {
-//   const { t } = useTranslation();
-//   const [isOpen, setIsOpen] = useState(true);
-
-//   const handleClosePopup = () => {
-//     setIsOpen(false);
-//     handlePopUp();
-//   };
-//   return (
-//     isOpen && (
-//       // <BackgroundMenuOpen>
-//       <PopUpSuccessContainer onClick={handleClosePopup}>
-//         <CloseSvgBtn type="button" onClick={handleClosePopup} />
-//         <SuccessMessage>{t('form-request')}</SuccessMessage>
-//         <ContactMessage>{t('form-contact')}</ContactMessage>
-//         <SuccessBtn type="button" onClick={handleClosePopup}>
-//           {t('form-perfect')}
-//         </SuccessBtn>
-//       </PopUpSuccessContainer>
-//       // </BackgroundMenuOpen>
-//     )
-//   );
-// };
 import React, { useState, useEffect, useCallback } from 'react';
 import {
   CloseSvgBtn,
